7e1a12db5dd85c9bd1a10034ee7cecd21e05d12e
Lerototi <lerototi@gmail.com>
2018-11-06 15:39:54 -0200

    Paginação Lançamentos

--- src/main/java/com/money/lerototi/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/money/lerototi/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,13 @@
 package com.money.lerototi.api.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.money.lerototi.api.model.Lancamento;
 import com.money.lerototi.api.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	
 }
--- src/main/java/com/money/lerototi/api/resource/LancamentoResource.java
+++ src/main/java/com/money/lerototi/api/resource/LancamentoResource.java
@@ -10,14 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.money.lerototi.api.event.RecursoCriadoEvent;
@@ -45,9 +49,9 @@ public class LancamentoResource {
 	private MessageSource messageSource;
 	
 	@GetMapping
-	public List<Lancamento> pesquisar(LancamentoFilter filter){
+	public Page<Lancamento> pesquisar(LancamentoFilter filter, Pageable pageable){
 		
-		return lancamentoRepository.filtrar(filter);
+		return lancamentoRepository.filtrar(filter, pageable);
 	}
 	
 	@GetMapping("/{codigo}")
@@ -65,6 +69,15 @@ public class LancamentoResource {
 		publish.publishEvent(new RecursoCriadoEvent(this, response, lancamentoRetornado.getCodigo()));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoRetornado);
+	}
+	
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletarLancamento(@PathVariable Long codigo) {
+		
+		lancamentoRepository.deleteById(codigo);
+		
 	}
 	
 	@ExceptionHandler({PessoaInexistenteOuInativaException.class})
