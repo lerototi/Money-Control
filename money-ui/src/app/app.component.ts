7d000b699fe8948b0fd5ac6acb39502af1ee07f3
Lerototi <lerototi@gmail.com>
2018-12-13 11:45:04 -0200

    Money-ui criação de projeto e página inicial.

---
+++ money-ui/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  lancamentos = [
+    {tipo: 'DESPESA', descricao: 'compra de pão', dataVencimento: '30/06/2017',
+    dataPagamento: '09/06/2017', valor: 4.55, pessoa: 'Atacado Brasil'}
+  ];
+}
---
+++ money-ui/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import {ButtonModule} from 'primeng/components/button/button';
+import {TableModule} from 'primeng/components/table/table';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
